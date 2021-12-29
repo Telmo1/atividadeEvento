4f8e2bfb442c23de6bbcf93cc038f3cf2da4242b
Telmo <yrtelmo00@gmail.com>
2021-12-28 23:15:01 -0300

    atividadeEento

---
+++ evento.js
@@ -0,0 +1,33 @@
+let diaDoEvento = 21122021
+
+
+if (diaDoEvento > 20122021) {
+    console.log("Data válida, continue o cadastro")
+
+} else {
+    console.log("Erro, data digitada invalida")
+}
+ let maiorIdade = 18
+
+ if (maiorIdade >= 18) {
+     console.log("Continue...")
+
+ } else {
+     console.log("É necessário te no mínimmo 18 anos de idade")
+ }
+
+ let listaDeParticipantes = 101
+
+
+ if (listaDeParticipantes < 100) {
+     console.log("Por favor adicione o nome a lista")
+    
+ } else {
+     console.log("Erro, a lista já está cheia")
+ }
+
+
+
+
+
+    
